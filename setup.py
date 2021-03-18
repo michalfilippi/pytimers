702a3d0a79b40483a3c64870da81ff393c7585ad
Michal Filippi <mfilippi@luc.id>
2021-03-18 20:35:41 +0100

    Version bump to 2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytimers",
-    version="2.0",
+    version="2.1",
     author="Michal Filippi",
     author_email="michal.filippi@gmail.com",
     description="Timing of functions, methods or block of codes made easy.",
