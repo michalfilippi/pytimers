6df701bf6ba1413e410a243c5e9ff1a7208d49dd
Michal Filippi <mfilippi@luc.id>
2022-02-11 23:09:17 +0100

    Add type annotations and docstrings to Timer

--- pytimers/timer.py
+++ pytimers/timer.py
@@ -4,7 +4,7 @@ import inspect
 from contextvars import ContextVar
 from timeit import default_timer
 from types import TracebackType
-from typing import Any, Callable, Iterable, Optional, Type
+from typing import Any, Awaitable, Callable, Iterable, Optional, Type
 
 from decorator import decorate  # type: ignore
 
@@ -13,34 +13,40 @@ from pytimers.triggers import BaseTrigger
 
 
 STARTED_CLOCK_VAR: ContextVar[Optional[list[StartedClock]]] = ContextVar(
-    "started_clock", default=None
+    "started_clock",
+    default=None,
 )
 
 
 class Timer:
     def __init__(
         self,
-        triggers: Iterable[
+        triggers: Optional[Iterable[
             BaseTrigger | Callable[[float, bool, Optional[str]], Any]
-        ] = None,
+        ]] = None,
     ):
-        """Initializes Timer object with custom configuration parameters.
+        """Initializes Timer object with a set of triggers to be applied after the
+        timer finishes.
 
-        :param triggers: A list of callables to be called after the timing finishes.
-            All triggers should accept keywords arguments duration: float, name: str,
-            code_block: bool.
+        :param triggers: An iterable of callables to be called after the timer finishes.
+            All triggers should accept keywords arguments duration_s: float,
+            decorator: bool, label: str. PyTimers also provide an abstract class
+            BaseTrigger to help with trigger interface implementation. See the
+            BaseTrigger for more details. Any instance of BaseTrigger subclass is a
+            valid trigger and can be passed to the triggers argument.
         """
 
         self._name: Optional[str] = None
-        self.triggers = triggers if triggers else []
+        self.triggers = list(triggers) if triggers else []
         self._latest_time: Optional[float] = None
 
     def label(self, name: str) -> "Timer":
-        """Sets name for the next timed code block. If there's no name set for code
-        blocks the log message will use general "code block" as a name for timed block.
+        """Sets label for the next timed code block. This label propagates to all
+        triggers once the context managers is closed.
 
-        :param name: Code block name.
-        :return: Returns self.
+        :param name: Code block label name.
+        :return: Returns self. This is comes makes possible to call the method directly
+            inside context manager with statement.
         """
 
         self._name = name
@@ -71,21 +77,31 @@ class Timer:
             started_timer.stop()
             self._finish_timing(started_timer.time, started_timer.label, False)
 
-    def _wrapper(self, wrapped, *args, **kwargs):
+    def _wrapper(
+            self,
+            wrapped: Callable[..., Any],
+            *args: Any,
+            **kwargs: Any,
+    ) -> Any:
         start_time = default_timer()
         output = wrapped(*args, **kwargs)
         end_time = default_timer()
         self._finish_timing(end_time - start_time, wrapped.__qualname__, True)
         return output
 
-    async def _async_wrapper(self, wrapped, *args, **kwargs):
+    async def _async_wrapper(
+            self,
+            wrapped: Callable[..., Awaitable[Any]],
+            *args: Any,
+            **kwargs: Any,
+    ) -> Any:
         start_time = default_timer()
         output = await wrapped(*args, **kwargs)
         end_time = default_timer()
         self._finish_timing(end_time - start_time, wrapped.__qualname__, True)
         return output
 
-    def __call__(self, wrapped):
+    def __call__(self, wrapped: Callable[..., Any]) -> Any:
         if inspect.iscoroutinefunction(wrapped):
             return decorate(wrapped, self._async_wrapper)
         else:
