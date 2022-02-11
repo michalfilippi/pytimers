5bb66f026616892c32fb6cebe524c766bd836381
Michal Filippi <mfilippi@luc.id>
2022-02-11 23:07:56 +0100

    Add docstrings to StartedClock

--- pytimers/started_clock.py
+++ pytimers/started_clock.py
@@ -10,16 +10,30 @@ class StartedClock:
         self.start_time = default_timer()
         self._duration: Optional[float] = None
 
-    def stop(self):
+    def stop(self) -> None:
+        """Stops the clock.
+        """
+
         self._duration = default_timer() - self.start_time
 
     @property
     def time(self) -> float:
+        """Exposes measured time of the clock.
+
+        :return: Measured time in seconds between start and stop of the clock.
+        :raise UnfinishedTimer: Clock has to be stopped before accessing elapsed time.
+        """
+
         if self._duration is None:
-            raise UnfinishedTimer()
+            raise UnfinishedTimer("Clock has to be stopped before accessing elapsed time.")
         else:
             return self._duration
 
     @property
     def current_duration(self) -> float:
+        """Calculates the duration elapsed since the clock started.
+
+        :return: Duration in seconds elapsed since the clock start.
+        """
+
         return default_timer() - self.start_time
