f9214e83474e43141f169371adc8eb123452773a
Michal Filippi <mfilippi@luc.id>
2022-02-11 16:39:14 +0100

    Improve logger trigger

--- pytimers/triggers/base_trigger.py
+++ pytimers/triggers/base_trigger.py
@@ -15,3 +15,19 @@ class BaseTrigger(ABC):
         label: str = None,
     ) -> None:
         pass
+
+    @staticmethod
+    def humanized_duration(duration_s: float, precision: int = 0) -> str:
+        hours, remainder = divmod(duration_s, 60 * 60)
+        minutes, remainder = divmod(remainder, 60)
+        seconds, remainder = divmod(remainder, 1)
+        ms = remainder * 1000
+
+        if hours > 0:
+            return f"{hours:.0f}h {minutes:.0f}m {seconds:.0f}s {ms:.{precision}f}ms"
+        elif minutes > 0:
+            return f"{minutes:.0f}m {seconds:.0f}s {ms:.{precision}f}ms"
+        elif seconds > 0:
+            return f"{seconds:.0f}s {ms:.{precision}f}ms"
+        else:
+            return f"{ms:.{precision}f}ms"
--- pytimers/triggers/logger_trigger.py
+++ pytimers/triggers/logger_trigger.py
@@ -10,12 +10,16 @@ class LoggerTrigger(BaseTrigger):
     def __init__(
         self,
         level: int = logging.INFO,
-        template: str = "Finished ${label} in ${duration}s.",
+        template: str = "Finished ${label} in ${humanized_duration} [${duration}s].",
+        precision: int = 3,
+        humanized_precision: int = 3,
     ):
         super().__init__()
         self.level = level
         self.logger = logging.getLogger(__name__)
         self.template = Template(template)
+        self.precision = precision
+        self.humanized_precision = humanized_precision
 
     def __call__(
         self,
@@ -26,7 +30,11 @@ class LoggerTrigger(BaseTrigger):
         self.logger.log(
             level=self.level,
             msg=self.template.substitute(
-                duration=duration_s,
+                duration=round(duration_s, self.precision),
+                humanized_duration=self.humanized_duration(
+                    duration_s=duration_s,
+                    precision=self.humanized_precision,
+                ),
                 label=label,
             ),
         )
