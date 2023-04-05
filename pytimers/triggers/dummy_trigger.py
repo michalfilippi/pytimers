b77d7071fc511f7ffd2eedbfa02b8c2543b6056a
Michal Filippi <michal.filippi@gmail.com>
2023-04-05 16:46:38 +0200

    Update docstring for dummy trigger

--- pytimers/triggers/dummy_trigger.py
+++ pytimers/triggers/dummy_trigger.py
@@ -6,6 +6,10 @@ from pytimers.triggers.base_trigger import BaseTrigger
 
 
 class DummyTrigger(BaseTrigger):
+    """This is a trigger implementation used for debugging and unit testing.
+    There's no real usage for this.
+    """
+
     def __init__(self) -> None:
         super().__init__()
         self.calls: list[tuple[float, bool, Optional[str]]] = []
